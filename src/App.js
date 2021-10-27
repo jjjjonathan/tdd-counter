044426a9c2fd4a159e380129adedf0a3e2e7fdb7
jjjjonathan <hornathanjon@gmail.com>
2021-10-27 13:13:42 -0700

    add initial value and increment tests

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0);
   return (
     <div className="App">
       <h1>This is counter app</h1>
-      <button id="increment-btn">Increment</button>
+      <div id="counter-value">{counter}</div>
+      <button id="increment-btn" onClick={() => setCounter(counter + 1)}>
+        Increment
+      </button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -6,13 +6,25 @@ import Adapter from 'enzyme-adapter-react-16';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('Counter Testing', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+
   test('renders the title of counter', () => {
-    const wrapper = shallow(<App />);
     expect(wrapper.find('h1').text()).toContain('This is counter app');
   });
 
   test('render a button with text of `increment`', () => {
-    const wrapper = shallow(<App />);
     expect(wrapper.find('#increment-btn').text()).toBe('Increment');
+  });
+
+  test('render the initial value of state in a div', () => {
+    expect(wrapper.find('#counter-value').text()).toBe('0');
+  });
+
+  test('render the click event of increment button and increment counter value', () => {
+    wrapper.find('#increment-btn').simulate('click');
+    expect(wrapper.find('#counter-value').text()).toBe('1');
   });
 });

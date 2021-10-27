93de4a578e2671e035ab4b209f5707a236f86283
jjjjonathan <hornathanjon@gmail.com>
2021-10-27 13:00:46 -0700

    add counter and button tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('Counter Testing', () => {
+  test('renders the title of counter', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('h1').text()).toContain('This is counter app');
+  });
+
+  test('render a button with text of `increment`', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('#increment-btn').text()).toBe('Increment');
+  });
 });
